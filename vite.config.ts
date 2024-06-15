803132423e967917887aa4b8ec373f1e8b26ed2f
Tyler <pyiphyo92@gmail.com>
2024-06-15 20:39:47 +0800

    Env Variable Setup for Index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv';
+import path from 'path';
 
+const envPath = path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`);
+console.log(`The current NODE_ENV is: ${process.env.NODE_ENV}`);
+dotenv.config({ path: envPath });
+// console.log("Path: ", envPath)
+
+// Custom plugin to inject environment variables into HTML
+const injectHtmlPlugin: Plugin = {
+  name: 'inject-html',
+  transformIndexHtml(html) {
+    return html.replace(
+      '<!-- inject:env -->',
+      `<script>
+         ${process.env.VITE_APP_SCRIPT}
+       </script>`
+    );
+  }
+};
+
+
+apiKeVITE_APP_SCRIPTy: process.env.VITE_APP_SCRIPT
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    injectHtmlPlugin
   ],
   resolve: {
     alias: {
@@ -14,3 +37,6 @@ export default defineConfig({
     }
   }
 })
+
+
+// console.log(`The current NODE_ENV is: ${process.env.NODE_ENV}`);
